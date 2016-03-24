395efa8b583ba9a42c4f022ad39329e1ef39061c
Travis <farhan687@gmail.com>
2016-03-24 03:26:06 +0000

    publish gh-pages (auto)

--- docbase-config.js
+++ docbase-config.js
@@ -21,29 +21,52 @@ var docbaseConfig = {
   "indexHtml": "./html/main.html",
   "flatdocHtml": "./bower_components/docbase/html/flatdoc.html",
   "html5mode": false,
-  "versions" : {
-    "v1": [{
-      "name": "sample",
-      "label": "Sample Label",
-      "files": [{
-        "name": "sample1",
-        "label": "Sample 1 Doc"
-      },]
-    }, {
-      "name": "howtostart",
-      "label": "How to start",
-      "files": [{
-        "name": "starting",
-        "label": "Starting with docbase"
-      }]
-    }],
-    "v2": [{
-      "name": "sample",
-      "label": "Sample Label",
-      "files": [{
-        "name": "sample1",
-        "label": "Sample 2 Doc"
-      }]
-    }]
+  "versions": {
+    "alpha": [
+      {
+        "label": "Helloworld",
+        "name": "Helloworld",
+        "files": [
+          {
+            "name": "welcome",
+            "label": "welcome"
+          },
+          {
+            "label": "index",
+            "name": "index"
+          }
+        ]
+      },
+      {
+        "label": "sampleWorld",
+        "name": "sampleWorld",
+        "files": [
+          {
+            "name": "justsample",
+            "label": "justsample"
+          },
+          {
+            "label": "index",
+            "name": "index"
+          }
+        ]
+      }
+    ],
+    "beta": [
+      {
+        "label": "sample",
+        "name": "sample",
+        "files": [
+          {
+            "name": "sample1",
+            "label": "sample1"
+          },
+          {
+            "label": "index",
+            "name": "index"
+          }
+        ]
+      }
+    ]
   }
-}
+};
