f0b4cc72222d843d4a146b2684185cd5a550a077
Farhan Chauhan <farhan687@gmail.com>
2016-03-30 19:03:48 +0530

    Megamenu testing

--- GruntFile.js
+++ GruntFile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
 					urlToAccess: "http://localhost:9001/",
 					assets: ['bower_components', 'styles', 'images', 'docbase-config.js'],
 					checkLoadedSelector: '#navbar-collapse',
-					endDocument: "<script>$(function(){  $('.search-form').searchAppbase('./search-index.json', true); setTimeout(function(){ $('#folder-navbar').megaMenu(); },200); })</script></html>"
+					endDocument: "<script>$(function(){  $('.search-form').searchAppbase('./search-index.json', true); $(document).ready(function(){ setTimeout(function(){ $('#folder-navbar').megaMenu(); },200); }); });</script></html>"
 				}
 			},
 			spa: {
