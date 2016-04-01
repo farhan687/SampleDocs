551d3e64882b526c7e1d27867f33e5243ba59c27
Farhan Chauhan <farhan687@gmail.com>
2016-04-01 15:59:31 +0530

    Update label

--- docbase-config.js
+++ docbase-config.js
@@ -21,30 +21,257 @@ var docbaseConfig = {
   "indexHtml": "./html/main.html",
   "flatdocHtml": "./bower_components/docbase/html/flatdoc.html",
   "html5mode": false,
-  "default_version": null,
-  "versions" : {
-    "v1": [{
-      "name": "sample",
-      "label": "Sample Label",
-      "files": [{
-        "name": "sample1",
-        "label": "Sample 1 Doc"
-      },]
-    }, {
-      "name": "howtostart",
-      "label": "How to start",
-      "files": [{
-        "name": "starting",
-        "label": "Starting with docbase"
-      }]
-    }],
-    "v2": [{
-      "name": "sample",
-      "label": "Sample Label",
-      "files": [{
-        "name": "sample1",
-        "label": "Sample 2 Doc"
-      }]
-    }]
+  "default_version": "scalr",
+  "versions": {
+    "scalr": [
+      {
+        "label": "Ironman",
+        "name": "FAQs",
+        "files": [
+          {
+            "name": "jsapi",
+            "label": "jsapi"
+          },
+          {
+            "name": "rest",
+            "label": "rest"
+          },
+          {
+            "label": "index",
+            "name": "index"
+          }
+        ]
+      },
+      {
+        "label": "Superman",
+        "name": "concepts",
+        "files": [
+          {
+            "name": "datamodel",
+            "label": "datamodel"
+          },
+          {
+            "name": "intro",
+            "label": "intro"
+          },
+          {
+            "label": "index",
+            "name": "index"
+          }
+        ]
+      },
+      {
+        "label": "Heman",
+        "name": "golang",
+        "files": [
+          {
+            "name": "api-reference",
+            "label": "api-reference"
+          },
+          {
+            "name": "golang-intro",
+            "label": "golang-intro"
+          },
+          {
+            "label": "index",
+            "name": "index"
+          }
+        ]
+      },
+      {
+        "label": "Spiderman",
+        "name": "javascript",
+        "files": [
+          {
+            "name": "api-reference",
+            "label": "api-reference"
+          },
+          {
+            "name": "javascript-intro",
+            "label": "javascript-intro"
+          },
+          {
+            "name": "javascript-webhooks",
+            "label": "javascript-webhooks"
+          },
+          {
+            "name": "meteor-intro",
+            "label": "meteor-intro"
+          },
+          {
+            "name": "nodejs-intro",
+            "label": "nodejs-intro"
+          },
+          {
+            "label": "index",
+            "name": "index"
+          }
+        ]
+      },
+      {
+        "label": "Captain America",
+        "name": "rest",
+        "files": [
+          {
+            "name": "intro",
+            "label": "intro"
+          },
+          {
+            "label": "index",
+            "name": "index"
+          }
+        ]
+      }
+    ],
+    "v2.0": [
+      {
+        "label": "concepts",
+        "name": "concepts",
+        "files": [
+          {
+            "name": "datamodel",
+            "label": "datamodel"
+          },
+          {
+            "label": "index",
+            "name": "index"
+          }
+        ]
+      },
+      {
+        "label": "javascript",
+        "name": "javascript",
+        "files": [
+          {
+            "name": "angular",
+            "label": "angular"
+          },
+          {
+            "name": "api-reference",
+            "label": "api-reference"
+          },
+          {
+            "name": "oauth",
+            "label": "oauth"
+          },
+          {
+            "name": "quick-start",
+            "label": "quick-start"
+          },
+          {
+            "label": "index",
+            "name": "index"
+          }
+        ]
+      },
+      {
+        "label": "rest",
+        "name": "rest",
+        "files": [
+          {
+            "name": "api-reference",
+            "label": "api-reference"
+          },
+          {
+            "label": "index",
+            "name": "index"
+          }
+        ]
+      },
+      {
+        "label": "search",
+        "name": "search",
+        "files": [
+          {
+            "name": "quick-start",
+            "label": "quick-start"
+          },
+          {
+            "name": "use-cases",
+            "label": "use-cases"
+          },
+          {
+            "label": "index",
+            "name": "index"
+          }
+        ]
+      }
+    ],
+    "v3.0": [
+      {
+        "label": "changelog",
+        "name": "changelog",
+        "files": [
+          {
+            "name": "changelog",
+            "label": "changelog"
+          },
+          {
+            "label": "index",
+            "name": "index"
+          }
+        ]
+      },
+      {
+        "label": "concepts",
+        "name": "concepts",
+        "files": [
+          {
+            "name": "datamodel",
+            "label": "datamodel"
+          },
+          {
+            "label": "index",
+            "name": "index"
+          }
+        ]
+      },
+      {
+        "label": "javascript",
+        "name": "javascript",
+        "files": [
+          {
+            "name": "api-reference",
+            "label": "api-reference"
+          },
+          {
+            "label": "index",
+            "name": "index"
+          }
+        ]
+      },
+      {
+        "label": "rest",
+        "name": "rest",
+        "files": [
+          {
+            "name": "api-reference",
+            "label": "api-reference"
+          },
+          {
+            "label": "index",
+            "name": "index"
+          }
+        ]
+      },
+      {
+        "label": "search",
+        "name": "search",
+        "files": [
+          {
+            "name": "quick-start",
+            "label": "quick-start"
+          },
+          {
+            "name": "use-cases",
+            "label": "use-cases"
+          },
+          {
+            "label": "index",
+            "name": "index"
+          }
+        ]
+      }
+    ]
   }
-}
+};
